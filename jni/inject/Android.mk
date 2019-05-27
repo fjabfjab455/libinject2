ee1807e0fc002df30602167f4ee812dca7ddd01b
Zeners <zeners.zou@irisian.com>
2019-05-27 14:59:30 +0800

    修改mk文件，防止权限问题导致的错误

--- jni/inject/Android.mk
+++ jni/inject/Android.mk
@@ -1,11 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := inject2Test
 LOCAL_SRC_FILES := inject2Test.c
 
 LOCAL_LDLIBS := -llog 
+LOCAL_LDFLAGS := -fPIE -pie
 
 #include $(BUILD_SHARED_LIBRARY)
 include $(BUILD_EXECUTABLE)
+
--- jni/inject_so/Android.mk
+++ jni/inject_so/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
+LOCAL_LDFLAGS := -static
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := hello
